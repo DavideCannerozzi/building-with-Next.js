61a3eb44d346ede8b017c0675f0aec318b24d9bb
davide <d.cannerozzi@gmail.com>
2022-09-15 11:00:01 +0200

    add project

---
+++ pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div>
+        <Head>
+            <title>About Page | Next.js</title>
+        </Head>
+        <h2>About Page</h2>
+    </div>
+  )
+}
---
+++ pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Contact() {
+  return (
+    <div>
+        <Head>
+            <title>Contact Page | Next.js</title>
+        </Head>
+        <h2>Contact Page</h2>
+    </div>
+  )
+}
